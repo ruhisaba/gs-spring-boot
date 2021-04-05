092f8d8b7fc93bf0fa397f4b570bc6d2358af7fb
Atib Samal <76742622+Harvey2504@users.noreply.github.com>
2021-04-05 15:43:43 +0530

    Update HelloController.java

--- app/src/main/java/hello/HelloController.java
+++ app/src/main/java/hello/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello from Srini!";
+        return "Deployments are Easy..! Phew ...!";
     }   
 }
